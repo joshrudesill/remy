d341963f7689c7e318928e4067aa641e961e59bb
Josh Rudesill <joshrudesill@Joshs-Mac-mini.local>
2025-08-16 17:25:02 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeroService } from './heroes/hero.service';
+import { NgIf } from '../../node_modules/@angular/common/common_module.d-NEF7UaHr';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'a1';
+  res: any[] = [];
+  loading = false;
+  m = 0;
+
+  s = inject(HeroService);
+
+  load() {
+    this.loading = true;
+    this.res = [];
+    this.s.get().subscribe({
+      next: (r: any) => {
+        console.log(r);
+        this.loading = false;
+        this.res = r;
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
+  }
 }
